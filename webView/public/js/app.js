402a46ab024389f248280426257cbd02bb6b9edb
HA_2k2 <hoanganh9637@gmail.com>
2022-12-21 14:07:36 +0700

    Xu ly duoc auto time

--- webView/public/js/app.js
+++ webView/public/js/app.js
@@ -27,72 +27,153 @@ function getHumColor(x) { //Lấy màu của độ ẩm  để hiển thị
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-
-    firebase.database().ref("Mode").on('value', (snapshot) => { //Set chức năng ko thao tác được khi ở chế độ auto
+    var humMax = 100
+    var humMin = 0
+    var tempMax
+    var tempMin
+    var modeValue
+    var speedValue
+    var tempGauge
+    var humGauge
+    var tempValue = 30
+    var humValue = 70
+    var timeStart
+    var timeStop
+    var timeBoard
+    var dateObj = new Date()
+    var month = dateObj.getUTCMonth() + 1; //months from 1-12
+    var day = dateObj.getUTCDate();
+    var year = dateObj.getUTCFullYear();
+    var DateNow = String(month + '/' + day + '/' + year)
+    var typeAUTO
+    firebase.database().ref('/DuLieuGuiXuongBoard/MODE').get().then((snapshot) => {
         if (snapshot.exists()) {
-            if (snapshot.val() == 1) {
-                document.body.innerHTML += '<div id= "overlay" style="bottom: 0;left: 0;position: fixed;right: 0;top: 0;"></div>'
-            }
-            if (snapshot.val() == 0) {
-                var element = document.getElementById('overlay');
-                if (typeof(element) != 'undefined' && element != null) { element.remove() }
-            }
-        } else {
-            console.log("No data available");
+            console.log(modeValue)
+            modeValue = snapshot.val()
+                // if (modeValue == 1) {
+                //     document.getElementById("mode").innerHTML = "Mode: By Hand" //Với trường hợp thao tác bằng tay => Thực hiện được thanh trượt, giới hạn nhiệt độ từ -20 - 60 , độ ẩm 0-100
+                //     tempGauge = createVerGauge('temp', -20, 60, ' °C')
+                //     humGauge = createRadGauge('hum', 0, 100, '%')
+                //     tempGauge.setVal(tempValue).setColor(getTempColor(tempValue))
+                //     humGauge.setVal(humValue).setColor(getHumColor(humValue))
+                //     document.getElementById("myRange").oninput = function() {
+                //         writeUserSpeedData(parseInt(this.value))
+                //     }
+                // } else if (modeValue == 0) {
+                //     document.getElementById("mode").innerHTML = "Mode: Auto"
+                //     tempGauge = createVerGauge('temp', tempMin, tempMax, ' °C')
+                //     humGauge = createRadGauge('hum', humMin, humMax, '%')
+                //     tempGauge.setVal(tempValue).setColor(getTempColor(tempValue))
+                //     humGauge.setVal(humValue).setColor(getHumColor(humValue))
+                // }
         }
-    });
+    })
+    firebase.database().ref('/DuLieuBoard/NhietDo').get().then((snapshot) => {
+        if (snapshot.exists()) {
+            tempValue = snapshot.val()
+        }
+    })
+    firebase.database().ref('/DuLieuBoard/DoAm').get().then((snapshot) => {
+        if (snapshot.exists()) {
+            humValue = snapshot.val()
+        }
+    })
 
-    firebase.database().ref().on('value', (snapshot) => { //Lấy dữ liệu realtime và xử lý
+    function writeUserSpeedData(value) { //Ghi trạng thái tốc độ vào server
+        firebase.database().ref('/DuLieuGuiXuongBoard/SPEED').set(value);
+    }
+    document.getElementById("myRange").oninput = function() {
+        writeUserSpeedData(parseInt(this.value))
+    }
+    firebase.database().ref("ThongSoBoard").on("value", (snapshot) => {
+        console.log("đã đọc thông số board")
         var _data = snapshot.val();
-        var ledStatus = _data['ledStatus'];
-        var tempValue = _data['NhietDo'];
-        var humValue = _data['DoAm'];
-        var speedValue = _data['Speed'];
-        var modeValue = _data['Mode'];
-        var humMax = _data['DoAmMax']
-        var humMin = _data['DoAmMin']
-        var tempMax = _data['NhietDoMax']
-        var tempMin = _data['NhietDoMin']
-        var dictspeedValue = { "0": "OFF", "25": "LOW", "50": "MEDIUM", "75": "HIGH", "100": "VERY HIGH" }
+        tempMax = _data["NhietDoMax"]
+        tempMin = _data["NhietDoMin"]
+        humMax = 100
+        humMin = 0
+        timeStart = Date.parse(DateNow + ' ' + _data["TimeStart"])
+        timeStop = Date.parse(DateNow + ' ' + _data["TimeStop"])
+        typeAUTO = _data["TYPE"]
+    })
+    firebase.database().ref("DuLieuGuiXuongBoard").on("value", (snapshot) => {
+        var _data = snapshot.val();
+        modeValue = _data['MODE']
+        if (modeValue == 1) {
+            document.getElementById("mode").innerHTML = "Mode: By Hand" //Với trường hợp thao tác bằng tay => Thực hiện được thanh trượt, giới hạn nhiệt độ từ -20 - 60 , độ ẩm 0-100
 
-        // Phần này tạo sự liên kết giữa các máy ( Khi máy này thay đổi thì máy kia cũng đổi)
-        // Cùng với đó cập nhật trạng thái lên các switch or slider
+        } else if (modeValue == 0) {
+
+            if (typeAUTO == 0) {
+                document.getElementById("mode").innerHTML = "Mode: By Auto Temp"
+            } else {
+                document.getElementById("mode").innerHTML = "Mode: By Auto Time"
+            }
+        }
+        speedValue = _data["SPEED"]
+        var dictspeedValue = { "0": "OFF", "25": "LOW", "50": "MEDIUM", "75": "HIGH", "100": "VERY HIGH" }
         document.getElementById("speedValue").innerHTML = dictspeedValue[String(speedValue)]
         document.getElementById("myRange").value = speedValue
-        document.getElementById("swtLed").checked = Boolean(ledStatus)
-        if (modeValue == 1) // Với hai trạng thái mode nên có if else
-            document.getElementById("mode").innerHTML = "Mode: Auto"
-        else if (modeValue == 0)
-            document.getElementById("mode").innerHTML = "Mode: By Hand"
+            // if (modeValue == 1) {
+            //     tempGauge = createVerGauge('temp', -20, 60, ' °C')
+            //     humGauge = createRadGauge('hum', 0, 100, '%')
+            //     tempGauge.setVal(tempValue).setColor(getTempColor(tempValue))
+            //     humGauge.setVal(humValue).setColor(getHumColor(humValue))
+            // } else if (modeValue == 0) {
+            //     tempGauge = createVerGauge('temp', tempMin, tempMax, ' °C')
+            //     humGauge = createRadGauge('hum', humMin, humMax, '%')
+            //     tempGauge.setVal(tempValue).setColor(getTempColor(tempValue))
+            //     humGauge.setVal(humValue).setColor(getHumColor(humValue))
+            // }
+    })
+    firebase.database().ref("DuLieuBoard").on("value", (snapshot) => {
+        console.log("Dữ liệu board đc cập nhật")
 
-        function writeUserSpeedData(value) { //Ghi trạng thái tốc độ vào server
-            firebase.database().ref('Speed').set(value);
-        }
-        if (ledStatus == 1) {
-            firebase.database().ref('Speed').set(0);
+        var _data = snapshot.val();
+        var FloatStatus = _data['FLOAT'];
+        tempValue = _data['NhietDo'];
+        humValue = _data['DoAm'];
+        timeBoard = Date.parse(DateNow + ' ' + _data["ThoiGian"].slice(0, 9))
+        document.getElementById("thoigiantrenboard").innerHTML = _data["ThoiGian"]
+        document.getElementById("swtLed").checked = 1 - Boolean(FloatStatus)
+
+        if (FloatStatus == 1) {
+            var dictspeedValue = { "0": "OFF", "25": "LOW", "50": "MEDIUM", "75": "HIGH", "100": "VERY HIGH" }
+            document.getElementById("speedValue").innerHTML = dictspeedValue[String(speedValue)]
+            document.getElementById("myRange").value = speedValue
         } else {
-            document.getElementById("myRange").oninput = function() {
-                console.log("Da truot")
-                writeUserSpeedData(parseInt(this.value))
-            };
+            document.getElementById("speedValue").innerHTML = "LOW"
+            document.getElementById("myRange").value = 0
         }
-        if (modeValue == 0) { //Với trường hợp thao tác bằng tay => Thực hiện được thanh trượt, giới hạn nhiệt độ từ -20 - 60 , độ ẩm 0-100
-            var tempGauge = createVerGauge('temp', -20, 60, ' °C')
-            var humGauge = createRadGauge('hum', 0, 100, '%')
-            document.getElementById("myRange").oninput = function() {
-                console.log("Da truot")
-                writeUserSpeedData(parseInt(this.value))
-            };
-        } else if (modeValue == 1) { //Với trường hợp thao tác tự động => Thực hiện theo giới hạn nhiệt độ được xét
-            var tempGauge = createVerGauge('temp', tempMin, tempMax, ' °C');
-            var humGauge = createRadGauge('hum', humMin, humMax, '%');
-            if (tempValue < tempMin || tempValue > tempMax) {
+        if (modeValue == 1) {
+            tempGauge = createVerGauge('temp', -20, 60, ' °C')
+            humGauge = createRadGauge('hum', 0, 100, '%')
+            tempGauge.setVal(tempValue).setColor(getTempColor(tempValue))
+            humGauge.setVal(humValue).setColor(getHumColor(humValue))
+        } else if (modeValue == 0) {
+            tempGauge = createVerGauge('temp', tempMin, tempMax, ' °C')
+            humGauge = createRadGauge('hum', humMin, humMax, '%')
+            tempGauge.setVal(tempValue).setColor(getTempColor(tempValue))
+            humGauge.setVal(humValue).setColor(getHumColor(humValue))
+            if ((tempValue < tempMin || tempValue > tempMax) && typeAUTO == 0 && FloatStatus == 1) {
                 writeUserSpeedData(50)
-            } else {
+            } else if ((tempValue > tempMin && tempValue < tempMax && typeAUTO == 0) || FloatStatus == 0) {
+                writeUserSpeedData(0)
+            }
+            // console.log(DateNow + ' ' + _data["ThoiGian"].slice(0, 9))
+            // console.log(timeBoard)
+            // console.log(timeStart)
+            // console.log(timeStop)
+            // console.log(FloatStatus)
+            // console.log(typeAUTO)
+            // console.log(timeBoard > timeStart && timeBoard < timeStop && typeAUTO == 1 && FloatStatus == 1)
+            // console.log(((timeBoard < timeStart || timeBoard > timeStop) && typeAUTO == 1) || FloatStatus == 0)
+            if (timeBoard > timeStart && timeBoard < timeStop && typeAUTO == 1 && FloatStatus == 1) {
+                writeUserSpeedData(50)
+            } else if (((timeBoard < timeStart || timeBoard > timeStop) && typeAUTO == 1) || FloatStatus == 0) {
                 writeUserSpeedData(0)
             }
         }
-        tempGauge.setVal(tempValue).setColor(getTempColor(tempValue));
-        humGauge.setVal(humValue).setColor(getHumColor(humValue));
+
     })
 })
--- webView/public/js/setting.js
+++ webView/public/js/setting.js
@@ -1,23 +1,49 @@
 document.addEventListener('DOMContentLoaded', function() { //Khi khởi tạo HTML thành công ko chờ các thành phần như stylesheet,images và subframes
 
-    firebase.database().ref().on('value', (snapshot) => { // Bấm nút update => Cập nhật lên server => Nhận thấy dữ liệu thay đổi => Cập nhật vào hiển thị màn hình
-        document.getElementById("mode").value = snapshot.val()['Mode']
+    firebase.database().ref('ThongSoBoard').get().then((snapshot) => { // Bấm nút update => Cập nhật lên server => Nhận thấy dữ liệu thay đổi => Cập nhật vào hiển thị màn hình
         document.getElementById("nhietdomax").value = String(snapshot.val()['NhietDoMax'])
         document.getElementById("nhietdomin").value = String(snapshot.val()['NhietDoMin'])
+        document.getElementById("caidatthoigianbatdau").value = String(snapshot.val()['TimeStart'])
+        document.getElementById("caidatthoigianketthuc").value = String(snapshot.val()['TimeStop'])
+        document.getElementById("mode_select").value = String(1 - snapshot.val()['TYPE'])
     })
-    document.getElementById("updateBtn").onclick = function() { // Thực hiện việc gửi lên server các thông số đã điền; khi xong hiển thị thông báo update thành công
-        firebase.database().ref('Mode').set(parseInt(document.getElementById("mode").value));
-        firebase.database().ref('NhietDoMax').set(parseFloat(document.getElementById("nhietdomax").value))
-        firebase.database().ref('NhietDoMin').set(parseFloat(document.getElementById("nhietdomin").value));
+    firebase.database().ref("DuLieuGuiXuongBoard").on('value', (snapshot) => {
+        document.getElementById("mode").value = snapshot.val()['MODE']
+    })
+    document.getElementById("updateBtn").onclick = function() {
+        firebase.database().ref('/ThongSoBoard/TYPE').set(1 - parseInt(document.getElementById("mode_select").value))
+        firebase.database().ref('/DuLieuGuiXuongBoard/MODE').set(parseInt(document.getElementById("mode").value))
+        if (document.getElementById("mode").value == 0) {
+            if (document.getElementById("mode_select").value == 0) {
+                console.log(document.getElementById("caidatthoigianbatdau").value)
+                console.log(document.getElementById("caidatthoigianketthuc").value)
+                firebase.database().ref('/ThongSoBoard/TimeStart').set(document.getElementById("caidatthoigianbatdau").value)
+                firebase.database().ref('/ThongSoBoard/TimeStop').set(document.getElementById("caidatthoigianketthuc").value)
+            } else {
+                firebase.database().ref('/ThongSoBoard/NhietDoMax').set(parseFloat(document.getElementById("nhietdomax").value))
+                firebase.database().ref('/ThongSoBoard/NhietDoMin').set(parseFloat(document.getElementById("nhietdomin").value));
+            }
+        }
         document.getElementById("showDone").style.display = "block"
+        firebase.database().ref('/DuLieuGuiXuongBoard/StateDown').set(1)
+        if (document.getElementById("mode").value == 0) {
+            firebase.database().ref('/DuLieuGuiXuongBoard/MODE').set(0);
+        } else if (document.getElementById("mode").value == 1) {
+            firebase.database().ref('/DuLieuGuiXuongBoard/MODE').set(1);
+        }
+        document.getElementById("showDone").innerHTML = "Đang xử lý ...(10s)"
     }
-    document.getElementById("mode").addEventListener("change", function() { // Mỗi khi bấm, chạm, thay đổi thì hiển thị thông báo update sẽ mất
-        document.getElementById("showDone").style.display = "none"
+    firebase.database().ref('/DuLieuBoard/Statesuccess').on('value', (snapshot) => {
+        if (snapshot.val() == 1) {
+            firebase.database().ref('/DuLieuGuiXuongBoard/StateDown').set(0)
+            document.getElementById("showDone").innerHTML = "Cập nhật thành công ..."
+        }
     })
 
     function showDoneFunc() {
         document.getElementById("showDone").style.display = "none"
     }
+    document.getElementById("mode").addEventListener("change", showDoneFunc)
     document.getElementById("nhietdomax").addEventListener("input", showDoneFunc);
     document.getElementById("nhietdomin").addEventListener("input", showDoneFunc);
 
